0e912062a4feb57ff6db5f335ece8a38d268cf7b
malis42 <mateuuszlis@gmail.com>
2020-09-07 15:14:15 +0200

    small fix user_table migration

--- migrations/migration0003_add_user_table.php
+++ migrations/migration0003_add_user_table.php
@@ -10,7 +10,7 @@ class migration0003_add_user_table
             `first_name` varchar(255) NOT NULL,
             `last_name` varchar(255) NOT NULL,
             `phone_number` varchar(255) NOT NULL, 
-            `address` varchar(255) NOT NULL,
+            `address_id` int(11) NOT NULL,
             PRIMARY KEY (`id`)
         );";
         $db->pdo->exec($sql);
